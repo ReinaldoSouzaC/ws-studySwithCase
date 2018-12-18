edd4c2196243d43926eced37067a1dfc32c19ed1
ReinaldoSouzaC <reinaldo723@outlook.com>
2018-12-18 11:09:56 -0300

    Para estudo

---
+++ src/swithCase/Swith.java
@@ -0,0 +1,32 @@
+package swithCase;
+
+import java.util.Scanner;
+
+public class Swith {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt(); //Ask the case
+		String caso;
+		
+		switch (x) { //Switch with value of variable
+			case 1: // Case...
+				caso = "Caso 1";
+				break;
+			case 2:
+				caso = "Caso 2";
+				break;
+			default: // Invalid case
+				caso = "Caso invalido";
+				break; // To break the switch
+		}
+		
+		System.out.println(caso); // Case = selected case
+		
+		sc.close();
+
+	}
+
+}
